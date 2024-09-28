8539401f9deaf0ba2eb21b4b8721c014fe9018f5
SkazDime <gef.brabu@gmail.com>
2024-09-28 11:04:28 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skazius-strength-tracker-v2';
+const CACHE_NAME = 'skazius-strength-tracker-v3';
 const urlsToCache = [
   'https://skazdime.github.io/SKAZMYO-TRACKER/',
   'https://skazdime.github.io/SKAZMYO-TRACKER/index.html',
